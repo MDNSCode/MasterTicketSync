f4ffbeb426855bdd4fd4c5ef227fd2e433dc7272
Matt Nixon <107679023+MDNSCode@users.noreply.github.com>
2025-04-22 07:29:54 +0100

    Update sync.php

--- inc/ticket/sync.php
+++ inc/ticket/sync.php
@@ -1,185 +1,101 @@
 <?php
 /**
- * Master Ticket Sync - Ticket Synchronization Logic
- * @author Matthew Nixon
- * @license MIT
+ * Master Ticket Sync - Core Logic
+ * @package MasterTicketSync
  */
 
-use Glpi\Toolbox\Log;
-use Glpi\Ticket\Ticket;
-use Glpi\Ticket\Followup as TicketFollowup;
-use Glpi\Ticket\Ticket_Ticket;
-use Glpi\ITILSolution\ITILSolution;
+if (!defined('GLPI_ROOT')) {
+    die("Direct access not allowed");
+}
 
-/**
- * Sync followups from parent to child tickets
- */
-function plugin_masterticketsync_followup_add(TicketFollowup $followup) {
-    global $DB;
+class PluginMasterticketsyncTicketSync extends CommonDBTM {
 
-    $ticket = new Ticket();
-    if (!$ticket->getFromDB($followup->fields['tickets_id'])) {
-        Log::write(
-            'Parent ticket not found for followup sync',
-            Log::ERROR,
-            ['followup_id' => $followup->getID()]
-        );
-        return;
+    static $rightname = 'plugin_masterticketsync';
+
+    static function getTypeName($nb = 0) {
+        return _n('Ticket Sync', 'Ticket Syncs', $nb, 'masterticketsync');
     }
 
-    try {
-        $childTickets = $DB->request([
-            'SELECT' => ['id'],
-            'FROM'   => 'glpi_tickets_tickets',
+    static function canCreate() {
+        return Session::haveRight('plugin_masterticketsync', CREATE);
+    }
+
+    static function handleNewTicket(Ticket $ticket) {
+        if (isset($ticket->input['_master_ticket_id'])) {
+            self::createRelation(
+                $ticket->input['_master_ticket_id'],
+                $ticket->getID()
+            );
+        }
+    }
+
+    static function handleTicketUpdate(Ticket $ticket) {
+        if (isset($ticket->input['_master_ticket_id'])) {
+            self::updateRelation(
+                $ticket->input['_master_ticket_id'],
+                $ticket->getID()
+            );
+        }
+    }
+
+    static function createRelation($masterID, $slaveID) {
+        global $DB;
+
+        if (!self::validateTickets($masterID, $slaveID)) {
+            return false;
+        }
+
+        $DB->insertOrDie('glpi_plugin_masterticketsync_relations', [
+            'master_ticket_id' => $masterID,
+            'slave_ticket_id' => $slaveID
+        ]);
+
+        Log::history(
+            $masterID,
+            'Ticket',
+            [0, '', sprintf(__('Linked to ticket %d', 'masterticketsync'), $slaveID)],
+            'PluginMasterticketsyncTicketSync'
+        );
+
+        return true;
+    }
+
+    static function validateTickets($masterID, $slaveID) {
+        $ticket = new Ticket();
+        
+        return $ticket->getFromDB($masterID) && 
+               $ticket->getFromDB($slaveID) &&
+               $masterID != $slaveID;
+    }
+
+    static function getTabNameForItem(CommonGLPI $item, $withtemplate = 0) {
+        if ($item->getType() == 'Ticket') {
+            return __('Master Sync', 'masterticketsync');
+        }
+        return '';
+    }
+
+    static function displayTabContentForItem(CommonGLPI $item, $tabnum = 1, $withtemplate = 0) {
+        if ($item->getType() == 'Ticket') {
+            self::showSyncInterface($item);
+            return true;
+        }
+        return false;
+    }
+
+    static function showSyncInterface(Ticket $ticket) {
+        global $DB;
+
+        $iterator = $DB->request([
+            'FROM'   => 'glpi_plugin_masterticketsync_relations',
             'WHERE'  => [
-                'tickets_id_1' => $ticket->getID(),
-                'link'        => Ticket_Ticket::SON_OF
+                'OR' => [
+                    ['master_ticket_id' => $ticket->getID()],
+                    ['slave_ticket_id' => $ticket->getID()]
+                ]
             ]
         ]);
 
-        foreach ($childTickets as $child) {
-            try {
-                $childFollowup = new TicketFollowup();
-                $childFollowup->add([
-                    'tickets_id'       => $child['id'],
-                    'content'          => $followup->fields['content'],
-                    'requesttypes_id'  => $followup->fields['requesttypes_id'],
-                    'is_private'       => $followup->fields['is_private'],
-                    'users_id'         => $followup->fields['users_id']
-                ]);
-            } catch (Exception $e) {
-                Log::write(
-                    'Failed to add followup to child ticket',
-                    Log::ERROR,
-                    [
-                        'parent_ticket' => $ticket->getID(),
-                        'child_ticket'  => $child['id'],
-                        'error'         => $e->getMessage()
-                    ]
-                );
-            }
-        }
-    } catch (Exception $e) {
-        Log::write(
-            'Failed to retrieve child tickets',
-            Log::CRITICAL,
-            [
-                'parent_ticket' => $ticket->getID(),
-                'error'         => $e->getMessage()
-            ]
-        );
+        // Render your interface here
     }
 }
-
-/**
- * Sync status changes from parent to child tickets
- */
-function plugin_masterticketsync_item_update(Ticket $ticket) {
-    global $DB;
-
-    if (!in_array('status', $ticket->updates)) {
-        return;
-    }
-
-    try {
-        $childTickets = $DB->request([
-            'SELECT' => ['id'],
-            'FROM'   => 'glpi_tickets_tickets',
-            'WHERE'  => [
-                'tickets_id_1' => $ticket->getID(),
-                'link'         => Ticket_Ticket::SON_OF
-            ]
-        ]);
-
-        foreach ($childTickets as $child) {
-            try {
-                $childTicket = new Ticket();
-                $childTicket->update([
-                    'id'     => $child['id'],
-                    'status' => $ticket->fields['status']
-                ]);
-            } catch (Exception $e) {
-                Log::write(
-                    'Failed to update child ticket status',
-                    Log::ERROR,
-                    [
-                        'parent_ticket' => $ticket->getID(),
-                        'child_ticket'  => $child['id'],
-                        'new_status'   => $ticket->fields['status'],
-                        'error'        => $e->getMessage()
-                    ]
-                );
-            }
-        }
-    } catch (Exception $e) {
-        Log::write(
-            'Failed to retrieve child tickets for status sync',
-            Log::CRITICAL,
-            [
-                'parent_ticket' => $ticket->getID(),
-                'error'         => $e->getMessage()
-            ]
-        );
-    }
-}
-
-/**
- * Sync solutions from parent to child tickets
- */
-function plugin_masterticketsync_solution_add(ITILSolution $solution) {
-    global $DB;
-
-    $ticket = new Ticket();
-    if (!$ticket->getFromDB($solution->fields['items_id'])) {
-        Log::write(
-            'Parent ticket not found for solution sync',
-            Log::ERROR,
-            ['solution_id' => $solution->getID()]
-        );
-        return;
-    }
-
-    try {
-        $childTickets = $DB->request([
-            'SELECT' => ['id'],
-            'FROM'   => 'glpi_tickets_tickets',
-            'WHERE'  => [
-                'tickets_id_1' => $ticket->getID(),
-                'link'         => Ticket_Ticket::SON_OF
-            ]
-        ]);
-
-        foreach ($childTickets as $child) {
-            try {
-                $childSolution = new ITILSolution();
-                $childSolution->add([
-                    'items_id'         => $child['id'],
-                    'itemtype'         => 'Ticket',
-                    'content'          => $solution->fields['content'],
-                    'solutiontypes_id' => $solution->fields['solutiontypes_id'],
-                    'users_id'        => $solution->fields['users_id']
-                ]);
-            } catch (Exception $e) {
-                Log::write(
-                    'Failed to add solution to child ticket',
-                    Log::ERROR,
-                    [
-                        'parent_ticket' => $ticket->getID(),
-                        'child_ticket'  => $child['id'],
-                        'error'         => $e->getMessage()
-                    ]
-                );
-            }
-        }
-    } catch (Exception $e) {
-        Log::write(
-            'Failed to retrieve child tickets for solution sync',
-            Log::CRITICAL,
-            [
-                'parent_ticket' => $ticket->getID(),
-                'error'         => $e->getMessage()
-            ]
-        );
-    }
-}
-?>
