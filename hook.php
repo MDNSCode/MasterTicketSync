6bd76bc06304eb609e8fcf93d9ad509826bdadfc
Matt Nixon <107679023+MDNSCode@users.noreply.github.com>
2025-04-22 07:00:31 +0100

    Update hook.php

--- hook.php
+++ hook.php
@@ -1,14 +1,62 @@
-function plugin_init_masterticketsync() {  // ← All lowercase
+<?php
+/**
+ * Master Ticket Sync plugin hooks
+ * 
+ * @package MasterTicketSync
+ * @license GPLv2+
+ */
+
+// Prevent direct access
+if (!defined('GLPI_ROOT')) {
+    die("Sorry - you can't access this file directly");
+}
+
+function plugin_init_masterticketsync() {
     global $PLUGIN_HOOKS;
+
+    // Required hooks
+    $PLUGIN_HOOKS['csrf_compliant']['masterticketsync'] = true;
     
-    $PLUGIN_HOOKS = [
-        'csrf_compliant' => ['masterticketsync' => true],  // ← Consistent lowercase
-        'menu_toadd'     => ['admin' => 'PluginMasterticketsyncMenu'],  // ← Add this
-        'config_page'    => ['masterticketsync' => 'front/config.form.php']  // ← Add if needed
+    // Add plugin to the admin menu
+    $PLUGIN_HOOKS['menu_toadd']['masterticketsync'] = [
+        'admin' => 'PluginMasterticketsyncMenu'
     ];
-    
-    // Register your ticket class
+
+    // Register the ticket class to show tabs on Ticket items
     Plugin::registerClass('PluginMasterticketsyncTicket', [
         'addtabon' => ['Ticket']
     ]);
+
+    // Add configuration page
+    $PLUGIN_HOOKS['config_page']['masterticketsync'] = 'front/config.form.php';
+
+    // Add plugin to helpdesk menu (if needed)
+    // $PLUGIN_HOOKS['menu_toadd']['masterticketsync'] = [
+    //     'helpdesk' => 'PluginMasterticketsyncMenu'
+    // ];
+
+    // Hook for when a ticket is shown
+    $PLUGIN_HOOKS['post_item_form']['masterticketsync'] = [
+        'Ticket' => 'plugin_masterticketsync_postshow'
+    ];
+
+    // Hook for when a ticket is updated
+    $PLUGIN_HOOKS['item_update']['masterticketsync'] = [
+        'Ticket' => 'plugin_masterticketsync_ticketupdate'
+    ];
+}
+
+function plugin_version_masterticketsync() {
+    return plugin_version_masterticketsync();
+}
+
+function plugin_masterticketsync_postshow($params) {
+    // Your code to execute after ticket form is shown
+}
+
+function plugin_masterticketsync_ticketupdate($item) {
+    // Your code to execute when a ticket is updated
+    if ($item->getType() == 'Ticket') {
+        // Handle ticket update logic
+    }
 }
